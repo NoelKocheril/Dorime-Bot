40e0c1a5aa6e45b3476a061298772789c91ecdd5
Noel Kocheril <NoelKocheril@Gmail.com>
2021-11-05 13:03:27 -0400

    Updated DorimeBot to match Class Structure

--- src/dorime-bot.py
+++ src/dorime-bot.py
@@ -29,73 +29,70 @@ if DEBUG:
     for key in config:
         print(f"{key}: {config[key]}")
 
-# Setup the Discord Client Object
-client = discord.Client()
-
-
+# Random Helper Functions
 def get_quote() -> str:
     res = requests.get("https://zenquotes.io/api/random").json()[0]
     return f"\"{res['q']}\" - {res['a']}"
 
 
-@client.event
-async def on_ready() -> None:
-    """Will display which user we are connecting to when the discord bot connects to the server"""
+# Dorime Bot Definitions
+class DorimeBot(discord.Client):
+    async def on_ready(self) -> None:
+        """Will display which user we are connecting to when the discord bot connects to the server"""
 
-    print(f"We Have Logged in as {client.user}")
+        print(f"We Have Logged in as {self.user}")
 
+    async def on_message(self, message: discord.Message) -> None:
+        """
+        Describes how to handle messages that the bot sees
 
-@client.event
-async def on_message(message: discord.Message) -> None:
-    """
-    Describes how to handle messages that the bot sees
+        :param discord.Message message: The incoming message
+        :return None
+        """
 
-    :param discord.Message message: The incoming message
-    :return None
-    """
-
-    # If the message is coming from the ignore...
-    if message.author == client.user:
-        return
-
-    author: discord.User = message.author
-    channel: discord.TextChannel = message.channel
-
-    if DEBUG:
-        print(
-            f"channel.id ({channel.id}) == SPOTIFY_WATCHED_CHANNEL ({SPOTIFY_WATCHED_CHANNEL}): {channel.id == SPOTIFY_WATCHED_CHANNEL}"
-        )
-
-    # If the message starts with $hello, reply with Hello
-    if channel.id == SPOTIFY_WATCHED_CHANNEL:
-        if DEBUG:
-            print("Message sent in Spotify Watch Channel")
-
-        spotify_track_ids = re.findall(SPOTIFY_URL_REGEX, message.content)
-
-        if DEBUG and not spotify_track_ids:
-            print("No Spotify Track URL found in Message")
+        # If the message is coming from the ignore...
+        if message.author == self.user:
             return
 
-        await message.channel.send(
-            f"Found the Spotify URL with the Track id(s): {spotify_track_ids}"
-        )
+        author: discord.User = message.author
+        channel: discord.TextChannel = message.channel
 
-        # TODO: Add Track id to Spotify Playlist
+        if DEBUG:
+            print(
+                f"channel.id ({channel.id}) == SPOTIFY_WATCHED_CHANNEL ({SPOTIFY_WATCHED_CHANNEL}): {channel.id == SPOTIFY_WATCHED_CHANNEL}"
+            )
 
-    elif message.content.startswith("$hello"):
-        msg = f"Hello <@{author.id}>!"
-        await message.channel.send(msg)
-    elif message.content.startswith("$inspire"):
-        quote = get_quote()
-        await message.channel.send(quote)
-    elif message.content.startswith("$here"):
+        # If the message starts with $hello, reply with Hello
+        if channel.id == SPOTIFY_WATCHED_CHANNEL:
+            if DEBUG:
+                print("Message sent in Spotify Watch Channel")
 
-        print(message)
+            spotify_track_ids = re.findall(SPOTIFY_URL_REGEX, message.content)
+
+            if DEBUG and not spotify_track_ids:
+                print("No Spotify Track URL found in Message")
+                return
+
+            await message.channel.send(
+                f"Found the Spotify URL with the Track id(s): {spotify_track_ids}"
+            )
+
+            # TODO: Add Track id to Spotify Playlist
+
+        elif message.content.startswith("$hello"):
+            msg = f"Hello <@{author.id}>!"
+            await message.channel.send(msg)
+        elif message.content.startswith("$inspire"):
+            quote = get_quote()
+            await message.channel.send(quote)
+        elif message.content.startswith("$here"):
+
+            print(message)
 
 
 def main():
     # Start the bot with Bot Token from the .env file
+    client = DorimeBot()
     client.run(config["DISCORD_TOKEN"])
 
 
